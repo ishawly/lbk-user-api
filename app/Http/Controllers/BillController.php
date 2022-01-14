17f79c7f302d01d752efaa6b261dee816073298c
liber <liber@shawly.cn>
2022-01-14 17:30:28 +0800

    test development environment

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+    public function index(Request $request)
+    {
+        return 'hello from bill index';
+    }
+}
