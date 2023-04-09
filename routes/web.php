9663dfec26f62ea4afea4d5fb2b7368bd2e4a046
liber <liber@shawly.cn>
2023-04-09 20:11:48 +0800

    remove unused files

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/bill/index', [\App\Http\Controllers\BillController::class, 'index']);
